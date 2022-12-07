36d5a6ab35636357633ddfe753f395a44b923105
Artem Nikiforov <artem169025@gmail.com>
2022-12-07 17:43:44 +0100

    Добавьте файлы проекта.

---
+++ Структура/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Структура
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  const string MyName = "Artem";
+            //Console.WriteLine(MyName);
+
+            // \n Символ перевода строки на новую строку
+            // \t табуляция (отступ, больше пробела)
+
+            //Console.WriteLine("Привет, \n Мир!");
+            //Console.WriteLine("Мне 32 года!");
+            //Console.WriteLine("My name is \n Artem");
+
+            // Вывод символов через таблицу ASII.
+
+            //Console.WriteLine("\u0040");
+            //Console.WriteLine("\x23");
+
+            //Console.ReadKey();
+
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+
+            Console.ReadKey();
+        }
+    }
+}
