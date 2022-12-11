36c3c21075a8ead7f76ae02287c0ebfef8732f94
Artem Nikiforov <artem169025@gmail.com>
2022-12-12 00:37:03 +0100

    Добавил Комментарии

--- Структура/Program.cs
+++ Структура/Program.cs
@@ -6,12 +6,12 @@ namespace Структура
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your Name: ");
+            Console.WriteLine("Enter your Name: "); //ВВедите Имя
             string name = Console.ReadLine();
-            Console.WriteLine("Enter your age: ");
+            Console.WriteLine("Enter your age: "); // ВВедите Возраст 
             byte age = (byte) int.Parse(Console.ReadLine());
-            Console.WriteLine("Your Name is {0} and your age is {1}",name,age);            
-            Console.WriteLine("Enter your Birthday: ");
+            Console.WriteLine("Your Name is {0} and your age is {1}",name,age);
+            Console.WriteLine("Your Birthday is: "); // Твой День Рождения
             var Birthday = Console.ReadLine();
             Console.WriteLine("Your Birthday is {0}",Birthday);
             Console.WriteLine("Well,your Name is {0} , your age is {1} and your Birthday is {2}",name,age,Birthday);
