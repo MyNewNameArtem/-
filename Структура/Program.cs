c2a7dd42b4379e4847e2d5ed9b24949f67f795de
Artem Nikiforov <artem169025@gmail.com>
2022-12-12 00:39:16 +0100

    Исправил строку 14

--- Структура/Program.cs
+++ Структура/Program.cs
@@ -11,7 +11,7 @@ namespace Структура
             Console.WriteLine("Enter your age: "); // ВВедите Возраст 
             byte age = (byte) int.Parse(Console.ReadLine());
             Console.WriteLine("Your Name is {0} and your age is {1}",name,age);
-            Console.WriteLine("Your Birthday is: "); // Твой День Рождения
+            Console.WriteLine("Enter your Birthday: "); // Введи твой День Рождения
             var Birthday = Console.ReadLine();
             Console.WriteLine("Your Birthday is {0}",Birthday);
             Console.WriteLine("Well,your Name is {0} , your age is {1} and your Birthday is {2}",name,age,Birthday);
